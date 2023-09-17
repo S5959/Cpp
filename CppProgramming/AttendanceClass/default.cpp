3390df984eb368983352ff41dbf41a2db556b999
HE210401 <geamie@naver.com>
2023-09-18 02:23:30 +0900

    [c++] 출석 과제물

---
+++ CppProgramming/AttendanceClass/default.cpp
@@ -0,0 +1,13 @@
+// 선행처리기 지시어를 통해 C++ 표준 입출력 라이브러리를 포함합니다.
+#include <iostream>
+
+// 'using' 구문을 사용하여 std 네임스페이스의 모든 요소를 명칭공간을 지정하지 않고 사용할 수 있습니다.
+using namespace std;
+
+// main 함수는 프로그램의 진입점입니다.
+int main()
+{
+    
+    // return은 함수를 종료하고 0을 반환합니다.
+    return 0;
+}
