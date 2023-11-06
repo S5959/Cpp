11a42d8346bbff9a62210bda0777f07ad9d41096
HE210401 <geamie@naver.com>
2023-11-06 17:59:26 +0900

    Cpp#4 함수 사용

---
+++ CppProgramming/Chapter_3/ConvFtoC.cpp
@@ -0,0 +1,27 @@
+/**
+ * p.82
+ * 소스코드 3-1
+*/
+
+#include<iostream>
+using namespace std;
+
+// 화씨온도를 섭씨온도로 변환하는 함수 : main() 함수보다 뒤에 있으면 오류남
+// 매개변수     float fahr : 화씨온도
+// 반환값      섭씨온도(float)
+float FahrToC(float fahr)
+{
+    return (fahr - 32) * 5 / 9;
+}
+
+int main()
+{
+    float fTemp, cTemp;
+
+    cout << "화씨 온도 : ";
+    cin >> fTemp;                                   // 화씨 온도 : 75
+    cTemp = FahrToC(fTemp);         // 함수호출
+    cout << "---> 섭씨 온도 : " <<  cTemp << endl;     // 섭씨 온도 : 23.8889
+    
+    return 0;
+}
