08f5453f2dbc73ce592ea023cfd1ceca115e8657
HE210401 <geamie@naver.com>
2023-11-08 20:50:44 +0900

    Cpp#3-4 값 호출

---
+++ CppProgramming/Chapter_3/FahrToC.cpp
@@ -0,0 +1,28 @@
+/**
+ * p.88
+ * 소스코드 3-4
+*/
+
+#include<iostream>
+using namespace std;
+
+// 화씨온도를 섭씨온도로 변환하는 함수 : main() 함수보다 뒤에 있으면 오류남
+// 매개변수     float fahr : 화씨온도
+// 반환값      섭씨온도(float)
+float FahrToC(float fahr)
+{
+    fahr -= 32;
+    return fahr * 5 / 9;
+}
+
+int main()
+{
+    float fTemp, cTemp;
+
+    cout << "화씨 온도 : ";
+    cin >> fTemp;                                   // 화씨 온도 : 75
+    cTemp = FahrToC(fTemp);         // 함수호출
+    cout << "---> 섭씨 온도 : " <<  cTemp << endl;     // 섭씨 온도 : 23.8889
+    
+    return 0;
+}
