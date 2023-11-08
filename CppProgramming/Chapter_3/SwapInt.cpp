48c5c161ba871d403e8a7e1971aa9f05a945c19a
HE210401 <geamie@naver.com>
2023-11-08 20:55:02 +0900

    Cpp#3-5 참조 호출

--- CppProgramming/Chapter_3/SwapInt.cpp
+++ CppProgramming/Chapter_3/SwapInt.cpp
@@ -1,15 +1,26 @@
+/**
+ * p.89 ~ p.90
+ * 소스코드 3-5
+*/
+
 #include<iostream>
 using namespace std;
+void SwapValues(int &x, int &y);    // 원형
 
 int main()
 {
-    int a = 10, b = 20;
-    if (a < b) SwapValues(a, b);
+    int a, b;
+    cout << "두 수를 입력하시오 : ";
+    cin >> a >> b;
+    if (a < b) SwapValues(a, b);        // 순서를 바꿔 a에 큰 값을 넣음
     cout << "큰 수 = " << a << " 작은 수 = " << b << endl;
 
     return 0;
 }
 
+// 두 변수의 값을 바꾸는 함수
+// 인수 int &x, int &y : 값을 바꿀 변수
+// 반환 : 없음
 void SwapValues(int& x, int&y)
 {
     int temp = x;
