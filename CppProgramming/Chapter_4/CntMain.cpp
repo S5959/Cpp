6814b8e7cad682bd9243dcc51688cd10e752273a
HE210401 <geamie@naver.com>
2023-11-13 19:26:48 +0900

    Cpp#4-4 생성자 예시

--- CppProgramming/Chapter_4/CntMain.cpp
+++ CppProgramming/Chapter_4/CntMain.cpp
@@ -1,6 +1,6 @@
 /**
- * 소스코드 4-2
- * p.120
+ * 소스코드 4-2, 소스코드 4-4
+ * p.120, p.125
 */
 
 #include<iostream>
@@ -9,8 +9,8 @@ using namespace std;
 
 int main()
 {
-    Counter cnt;        // Counter 객체의 정의
-    cnt.reset();        // 계수기를 0으로 지움
+    Counter cnt;        // Counter 객체의 정의 - value는 자동적으로 0이 됨
+    // cnt.reset();        // 계수기를 0으로 지움 - 생성자로 초기화하여 생략 가능
     cout << "계수기의 현재 값: " << cnt.getValue() << endl;
     cnt.count();        // 계수기를 1 증가시킴
     cnt.count();
