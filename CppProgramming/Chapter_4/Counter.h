06612bae8af12752df6623e8e5745f76af12117f
HE210401 <geamie@naver.com>
2023-11-13 19:31:34 +0900

    Cpp#4-5 생성자 초기화

--- CppProgramming/Chapter_4/Counter.h
+++ CppProgramming/Chapter_4/Counter.h
@@ -1,6 +1,5 @@
 /**
- * 소스코드 4-1, 소스코드 4-3
- * p.118, p.124
+ * 소스코드 4-1(p.118), 소스코드 4-3(p.124), 소스코드 4-5(p.125)
 */
 #ifndef COUNTER_H_INCLUDED     // Counter.h가 중복 include 되지 않았는지 검사
 
@@ -9,8 +8,9 @@
 class Counter {     // 클래스 Counter의 선언 시작
     int value;      // private 데이터 멤버
 public:             // public 멤버 변수
-    Counter()       // 생성자
-        { value = 0; }
+    Counter() : value(0) {}   //생성자 - 교재에는 value{ 0 } {} 표기되어 있지만 에러남
+    // Counter()       // 생성자
+    //     { value = 0; }
     void reset()            // 계수기의 값을 0으로 지움
         { value = 0; }
     void count()            // 계수기의 값을 1 증가시킴
