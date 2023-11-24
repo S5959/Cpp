85d184ded5c8977cf480eaff1e4822369e27fabe
HE210401 <geamie@naver.com>
2023-11-24 19:09:55 +0900

    Cpp#5-7 ~ 13 이항 산술 연산자 및 관계 연산자의 다중정의

---
+++ CppProgramming/Chapter_5/Complex2.cpp
@@ -0,0 +1,52 @@
+/***
+ * 소스코드 5-7(p.185), 5-8(p.186), 5-9(p.186), 5-10(p.189), 5-11(p.189), 5-12(p.190), 5-13(p.191)
+*/
+#include <iostream>
+#include "Complex2.h"
+
+// 소스코드 5-7
+Complex2 Complex2::operator+(const Complex2& c) const
+{
+    Complex2 tmp(*this);
+    tmp.rPart += c.rPart;
+    tmp.iPart += c.iPart;
+    return tmp;
+}
+
+// 소스코드 5-8
+Complex2 Complex2::operator+(const Complex2& c) const
+{
+    return Complex2(rPart + c.rPart, iPart + c.iPart);
+}
+
+// 소스코드 5-9
+Complex2 Complex2::operator+(double r) const
+{
+    return Complex2(rPart + r, iPart);
+}
+
+// 소스코드 5-10
+// Complex2 operator+(double r, const Complex2& c)
+// {
+//     return Complex2(r + c.rPart(), c.iPart());      // 오류 발생 : rPart, iPart는 c의 private 데이터 멤버
+// }
+
+// 소스코드 5-11
+Complex2 operator+(double r, const Complex2& c)
+{
+    return Complex2(r + c.real(), c.imag());
+}
+
+// 소스코드 5-12 (코드 수정본)
+Complex2& Complex2::operator+=(const Complex2& c)
+{
+    this->rPart += c.rPart;
+    this->iPart += c.iPart;
+    return *this;
+}
+
+// 소스코드 5-13
+bool Complex2::operator==(const Complex2& c) const
+{
+    return rPart == c.rPart && iPart == c.iPart;
+}
--- CppProgramming/Chapter_5/Complex2.h
+++ CppProgramming/Chapter_5/Complex2.h
@@ -1,5 +1,5 @@
 /***
- * 소스코드 5-6 (p.183)
+ * 소스코드 5-6 (p.183), 5-10(p.189), 5-11(p.189), 소스코드 5-12(p.190)
 */
 #ifndef COMPLEX2_H_INCLUDED
 #define COMPLEX2_H_INCLUDED
@@ -15,6 +15,16 @@ public:
         return Complex2(rPart, -iPart);
     }
     Complex2 operator+(const Complex2& c) const;
+    Complex2 operator+(double r) const;
+    // 소스코드 5-10 설명
+    double real() const { return rPart; }   // 실수부의 값 반환
+    double imag() const { return iPart; }   // 허수부의 값 반환
+    // 소스코드 5-11 설명
+    friend Complex2 operator+(double r, const Complex2& c);
+    // 소스코드 5-12
+    Complex2& operator+=(const Complex2& c);
+    // 소스코드 5-13
+    bool operator==(const Complex2& c) const;
 };
 
 #endif
